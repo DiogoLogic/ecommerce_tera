6ae9002ef95d79fbbbbcb5ac70b6a717d8c86b39
DiogoLogic <diogologic@gmail.com>
2023-11-05 19:10:49 -0300

    adcionando menu componente  sheet

--- tera/src/components/ui/header.tsx
+++ tera/src/components/ui/header.tsx
@@ -1,21 +1,32 @@
-import {  MenuIcon, ShoppingCart, icons } from "lucide-react";
+import { MenuIcon,  ShoppingCart} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetTrigger } from "./sheet";
+
 
 const Header = () => {
   return (
     <Card className="flex justify-between p-[1.875rem] items-center">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        
+        <SheetContent side="left">
+            <h1>Menu</h1>
+        </SheetContent>
+        
+      </Sheet>
 
       <h1 className="font-semibold text-green-500">
         <span className="text-primary ">Ecommerce </span>Tera
-        </h1>
+      </h1>
       <Button size="icon" variant="outline">
         <ShoppingCart />
       </Button>
-
     </Card>
   );
 };
